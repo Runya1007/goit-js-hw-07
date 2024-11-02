2f20ea10aa8489ed1069fd0c5f5244ff5dabd7dd
Ruslan Shevchenko <runya_@ukr.net>
2024-11-02 12:56:42 +0200

    added task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+
+const handleClick = () => {
+  const color = getRandomHexColor();
+  span.textContent = color;
+  body.style.backgroundColor = color;
+};
+
+button.addEventListener("click", handleClick);
